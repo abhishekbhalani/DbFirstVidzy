244226a6ba580f84e4974480b20834d1f17175c1
Thomas <voets_thomas@hotmail.com>
2018-02-10 21:05:43 +0100

    Add project files.

---
+++ DbFirst Exercise Vidzy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst_Exercise_Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
